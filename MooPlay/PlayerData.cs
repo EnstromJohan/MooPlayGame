427a557a9c21004366f780d3c283027ae94d0fac
EnstromJohan <johan.enstrom@mail.com>
2023-10-16 17:41:05 +0200

    playerdata

--- MooPlay/PlayerData.cs
+++ MooPlay/PlayerData.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MooPlay
+﻿namespace MooPlay
 {
-    internal class PlayerData
+    public class PlayerData
     {
+        public string Name { get; private set; }
+        public int GamesPlayed { get; private set; }
+        private int TotalGuesses;
+
+        public PlayerData(string name, int totalGuesses)
+        {
+            Name = name;
+            GamesPlayed = 1;
+            TotalGuesses = totalGuesses;
+        }
+
+        public void Update(int totalGuesses)
+        {
+            TotalGuesses += totalGuesses;
+            GamesPlayed++;
+        }
+
+        public double Average()
+        {
+            if (GamesPlayed > 0)
+            {
+                return (double)TotalGuesses / GamesPlayed;
+            }
+
+            else
+            {
+                return 0.0;
+            }
+        }
     }
 }
+
